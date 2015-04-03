59642e38cdd5d913322d64fcd3113a7a1adfa08d
Soulsby <jwsoulsby@gmail.com>
2015-04-03 13:48:21 -0400

    Got my Java fixed

--- TicketToRide/src/Deck.java
+++ TicketToRide/src/Deck.java
@@ -1,4 +1,8 @@
 
 public class Deck {
-
+	public int cards;
+	public void Deck(){
+		cards=110;
+	}
+	
 }
--- TicketToRide/src/DeckTest.java
+++ TicketToRide/src/DeckTest.java
@@ -1,13 +1,18 @@
 import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 
 public class DeckTest {
-
+	Deck testDeck = new Deck();
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDeckConstructor() {
+		assertEquals(testDeck.cards,110);
 	}
-
+	
+	@Test
+	public void testPlayerDeal() {
+		
+	}
 }
