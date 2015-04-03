f5a38f46d0c173b1f6eb0537c3b96987350ed629
Arcessimus <dsfreak17@gmail.com>
2015-04-03 12:51:28 -0400

    Deck class unit tests

--- TicketToRide/src/Deck.java
+++ TicketToRide/src/Deck.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+
 
 public class Deck {
+	
+	ArrayList<ICard> deck;
+
+	public void shuffle() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ICard getCard(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- TicketToRide/src/DeckTest.java
+++ TicketToRide/src/DeckTest.java
@@ -3,11 +3,58 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class DeckTest {
+public class DeckTest {	
+	
+	@Test
+	public void testInit() {
+		Deck main = new Deck();
+		assertNotNull(main);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testShuffle() {
+		Deck test = new Deck();
+		test.shuffle();
+		
+		boolean inOrder = true;
+		
+		for(int i = 0; i < test.getSize(); i++) {
+			if(test.getCard(i).getID() > test.getCard(i+1).getID()) {
+				inOrder = false;
+				break;
+			}
+		}
+		
+		assertFalse(inOrder);
+	}
+	
+	@Test
+	public void testShuffleReverse() {
+		Deck test = new Deck();
+		test.shuffle();
+		
+		boolean inOrder = true;
+		
+		for(int i = 0; i < test.getSize(); i++) {
+			if(test.getCard(i).getID() < test.getCard(i+1).getID()) {
+				inOrder = false;
+				break;
+			}
+		}
+		
+		assertFalse(inOrder);
+	}
+	
+	@Test
+	public void testThatDrawRemovesOneCard() {
+		Deck test = new Deck();
+		int cursize = test.getSize();
+		int expsize = cursize - 1;
+		
+		test.draw();
+		
+		cursize = test.getSize();
+		assertEquals(expsize, cursize);
 	}
 
 }
