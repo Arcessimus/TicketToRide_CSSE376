5fe55a62ca220b1f3c7ca97f3cb025b9cb467d0a
Soulsby <jwsoulsby@gmail.com>
2015-04-20 16:09:29 -0400

    TrainCardTest Passed
    I'm not really sure where I can start into the Deck test.

--- TicketToRide/src/TrainCard.java
+++ TicketToRide/src/TrainCard.java
@@ -2,12 +2,12 @@
 public class TrainCard implements ICard {
 	public static enum CARD_TYPE {RED, BLACK,GREEN,YELLOW,BLUE,PURPLE,ORANGE,WHITE,WILD
 	}
-	public TrainCard(CARD_TYPE red) {
-		// TODO Auto-generated constructor stub
-	}
+	public CARD_TYPE cardType;
 	public int getID() {
 		return 0;
 	}
-	public CARD_TYPE cardType;
 	
+	public TrainCard(CARD_TYPE type) {
+		cardType=type;
+	}
 }
