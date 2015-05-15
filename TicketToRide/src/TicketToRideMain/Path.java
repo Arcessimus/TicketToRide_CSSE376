7db2609e8ec866ef3069c40f5c630df0c00b0c9b
Arcessimus <dsfreak17@gmail.com>
2015-05-15 12:47:56 -0400

    more work on routes and paths

--- TicketToRide/src/TicketToRideMain/Path.java
+++ TicketToRide/src/TicketToRideMain/Path.java
@@ -1,7 +1,12 @@
 package TicketToRideMain;
 
+import java.util.ArrayList;
+
 //the purpose of this class is to help calculate bonuses acquired through fulfilling, or not fulfilling
 //destination cards
 public class Path {
+	
+	//card that this path corresponds to
+	ArrayList<Route> routesInPath;
 
 }
--- TicketToRide/src/TicketToRideMain/Route.java
+++ TicketToRide/src/TicketToRideMain/Route.java
@@ -3,5 +3,10 @@ package TicketToRideMain;
 
 //the purpose of this class is to calculate individual, singular routes between two adjacent cities
 public class Route {
-
+	
+	Player owner;
+	//some way of determining color of the route
+	int pointValue;
+	//the two cities that this route connects
+	
 }
