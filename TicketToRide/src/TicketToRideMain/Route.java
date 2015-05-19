4deeeb62f373417b2d1e30944b6e049ab98bb98f
Arcessimus <dsfreak17@gmail.com>
2015-05-19 14:49:00 -0400

    some work on routes

--- TicketToRide/src/TicketToRideMain/Route.java
+++ TicketToRide/src/TicketToRideMain/Route.java
@@ -1,12 +1,45 @@
 package TicketToRideMain;
 
+import java.util.ArrayList;
+
 
 //the purpose of this class is to calculate individual, singular routes between two adjacent cities
 public class Route {
 	
 	Player owner;
 	String color;
-	int pointValue;
-	//the two cities that this route connects
+	int pointValue; 
+	ArrayList<City> cities;
+	int length;
+	
+	boolean hasTwin;
+	boolean smallGame;
+	int owned;
+	Route twin;
+	
+	public Route(City a, City b, int ptVal, String color, int length)
+	{
+		this.cities = new ArrayList<City>();
+		this.cities.add(a);
+		this.cities.add(b);
+		
+		this.pointValue = ptVal;
+		this.color = color;
+		this.length = length;
+	}
+	
+	public void setOwner(Player p) {
+		this.owner = p;
+		this.setOwned(1);
+		
+		if(this.smallGame && this.hasTwin) {
+			this.twin.setOwned(-1);
+		}
+	}
+
+	private void setOwned(int i) {
+		this.owned = i;
+	}
+	
 	
 }
