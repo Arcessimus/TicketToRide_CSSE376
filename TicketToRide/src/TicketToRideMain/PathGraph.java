2353b1d0a7aa319c82a42b407743d0255b1c49b0
Arcessimus <dsfreak17@gmail.com>
2015-05-22 03:30:54 -0400

    Routes now account for small or larger game

--- TicketToRide/src/TicketToRideMain/PathGraph.java
+++ TicketToRide/src/TicketToRideMain/PathGraph.java
@@ -27,6 +27,9 @@ public class PathGraph {
 	}
 	
 	private void init() {
+		//adjust for number of players
+		boolean smallGame = this.players.size() < 4;
+		
 		//initialize all cities and put them into list
 		City Boston = new City("Boston");
 		this.cities.add(Boston);
@@ -101,65 +104,65 @@ public class PathGraph {
 		City Omaha = new City("Omaha");
 		this.cities.add(Omaha);
 		
-		// TODO initialize all routes and put them into list
+		//initialize all routes and put them into list
 		
 		//single routes
-		Route one = new Route(Vancouver, Calgary, 0, "", 0, true);
-		Route two = new Route(Calgary, Seattle, 0, "", 0, true);
-		Route three = new Route(Calgary, Helena, 0, "", 0, true);
-		Route four = new Route(Calgary, Winnipeg, 0, "", 0, true);
-		Route five = new Route(Seattle, Helena, 0, "", 0, true);
-		Route six = new Route(Portland, SaltLakeCity, 0, "", 0, true);
-		Route seven = new Route(SaltLakeCity, LasVegas, 0, "", 0, true);
-		Route eight = new Route(SaltLakeCity, Helena, 0, "", 0, true);
-		Route nine = new Route(LosAngeles, LasVegas, 0, "", 0, true);
-		Route ten = new Route(LosAngeles, Phoenix, 0, "", 0, true);
-		Route eleven = new Route(LosAngeles, ElPaso, 0, "", 0, true);
-		Route twelve = new Route(Phoenix, ElPaso, 0, "", 0, true);
-		Route thirteen = new Route(Phoenix, Denver, 0, "", 0, true);
-		Route fourteen = new Route(Phoenix, SantaFe, 0, "", 0, true);
-		Route fifteen = new Route(ElPaso, Houston, 0, "", 0, true);
-		Route sixteen = new Route(ElPaso, Dallas, 0, "", 0, true);
-		Route seventeen = new Route(ElPaso, OklahomaCity, 0, "", 0, true);
-		Route eighteen = new Route(ElPaso, SantaFe, 0, "", 0, true);
-		Route nineteen = new Route(Houston, NewOrleans, 0, "", 0, true);
-		Route twenty = new Route(Dallas, LittleRock, 0, "", 0, true);
-		Route twentyone = new Route(NewOrleans, LittleRock, 0, "", 0, true);
-		Route twentytwo = new Route(NewOrleans, Miami, 0, "", 0, true);
-		Route twentythree = new Route(Denver, OklahomaCity, 0, "", 0, true);
-		Route twentyfour = new Route(Denver, Omaha, 0, "", 0, true);
-		Route twentyfive = new Route(Denver, Helena, 0, "", 0, true);
-		Route twentysix = new Route(Denver, SantaFe, 0, "", 0, true);
-		Route twentyseven = new Route(OklahomaCity, LittleRock, 0, "", 0, true);
-		Route twentyeight = new Route(OklahomaCity, SantaFe, 0, "", 0, true);
-		Route twentynine = new Route(LittleRock, StLouis, 0, "", 0, true);
-		Route thirty = new Route(LittleRock, Nashville, 0, "", 0, true);
-		Route thirtyone = new Route(StLouis, Nashville, 0, "", 0, true);
-		Route thirtytwo = new Route(StLouis, Pittsburgh, 0, "", 0, true);
-		Route thirtythree = new Route(Omaha, Chicago, 0, "", 0, true);
-		Route thirtyfour = new Route(Omaha, Helena, 0, "", 0, true);
-		Route thirtyfive = new Route(Atlanta, Charleston, 0, "", 0, true);
-		Route thirtysix = new Route(Atlanta, Miami, 0, "", 0, true);
-		Route thirtyseven = new Route(Atlanta, Nashville, 0, "", 0, true);
-		Route thirtyeight = new Route(Charleston, Raleigh, 0, "", 0, true);
-		Route thirtynine = new Route(Charleston, Miami, 0, "", 0, true);
-		Route fourty = new Route(Raleigh, Nashville, 0, "", 0, true);
-		Route fourtyone = new Route(Raleigh, Pittsburgh, 0, "", 0, true);
-		Route fourtytwo = new Route(Nashville, Pittsburgh, 0, "", 0, true);
-		Route fourtythree = new Route(Washington, Pittsburgh, 0, "", 0, true);
-		Route fourtyfive = new Route(Pittsburgh, Toronto, 0, "", 0, true);
-		Route fourtysix = new Route(NewYork, Montreal, 0, "", 0, true);
-		Route fourtyseven = new Route(Montreal, SaultStMarie, 0, "", 0, true);
-		Route fourtyeight = new Route(Montreal, Toronto, 0, "", 0, true);
-		Route fourtynine = new Route(SaultStMarie, Toronto, 0, "", 0, true);
-		Route fifty = new Route(SaultStMarie, Winnipeg, 0, "", 0, true);
-		Route fiftyone = new Route(SaultStMarie, Duluth, 0, "", 0, true);
-		Route fiftytwo = new Route(Toronto, Chicago, 0, "", 0, true);
-		Route fiftythree = new Route(Toronto, Duluth, 0, "", 0, true);
-		Route fiftyfour = new Route(Chicago, Duluth, 0, "", 0, true);
-		Route fiftyfive = new Route(Helena, Winnipeg, 0, "", 0, true);
-		Route fiftysix = new Route(Helena, Duluth, 0, "", 0, true);
-		Route fiftyseven = new Route(Winnipeg, Duluth, 0, "", 0, true);
+		Route one = new Route(Vancouver, Calgary, 0, "", 0, smallGame);
+		Route two = new Route(Calgary, Seattle, 0, "", 0, smallGame);
+		Route three = new Route(Calgary, Helena, 0, "", 0, smallGame);
+		Route four = new Route(Calgary, Winnipeg, 0, "", 0, smallGame);
+		Route five = new Route(Seattle, Helena, 0, "", 0, smallGame);
+		Route six = new Route(Portland, SaltLakeCity, 0, "", 0, smallGame);
+		Route seven = new Route(SaltLakeCity, LasVegas, 0, "", 0, smallGame);
+		Route eight = new Route(SaltLakeCity, Helena, 0, "", 0, smallGame);
+		Route nine = new Route(LosAngeles, LasVegas, 0, "", 0, smallGame);
+		Route ten = new Route(LosAngeles, Phoenix, 0, "", 0, smallGame);
+		Route eleven = new Route(LosAngeles, ElPaso, 0, "", 0, smallGame);
+		Route twelve = new Route(Phoenix, ElPaso, 0, "", 0, smallGame);
+		Route thirteen = new Route(Phoenix, Denver, 0, "", 0, smallGame);
+		Route fourteen = new Route(Phoenix, SantaFe, 0, "", 0, smallGame);
+		Route fifteen = new Route(ElPaso, Houston, 0, "", 0, smallGame);
+		Route sixteen = new Route(ElPaso, Dallas, 0, "", 0, smallGame);
+		Route seventeen = new Route(ElPaso, OklahomaCity, 0, "", 0, smallGame);
+		Route eighteen = new Route(ElPaso, SantaFe, 0, "", 0, smallGame);
+		Route nineteen = new Route(Houston, NewOrleans, 0, "", 0, smallGame);
+		Route twenty = new Route(Dallas, LittleRock, 0, "", 0, smallGame);
+		Route twentyone = new Route(NewOrleans, LittleRock, 0, "", 0, smallGame);
+		Route twentytwo = new Route(NewOrleans, Miami, 0, "", 0, smallGame);
+		Route twentythree = new Route(Denver, OklahomaCity, 0, "", 0, smallGame);
+		Route twentyfour = new Route(Denver, Omaha, 0, "", 0, smallGame);
+		Route twentyfive = new Route(Denver, Helena, 0, "", 0, smallGame);
+		Route twentysix = new Route(Denver, SantaFe, 0, "", 0, smallGame);
+		Route twentyseven = new Route(OklahomaCity, LittleRock, 0, "", 0, smallGame);
+		Route twentyeight = new Route(OklahomaCity, SantaFe, 0, "", 0, smallGame);
+		Route twentynine = new Route(LittleRock, StLouis, 0, "", 0, smallGame);
+		Route thirty = new Route(LittleRock, Nashville, 0, "", 0, smallGame);
+		Route thirtyone = new Route(StLouis, Nashville, 0, "", 0, smallGame);
+		Route thirtytwo = new Route(StLouis, Pittsburgh, 0, "", 0, smallGame);
+		Route thirtythree = new Route(Omaha, Chicago, 0, "", 0, smallGame);
+		Route thirtyfour = new Route(Omaha, Helena, 0, "", 0, smallGame);
+		Route thirtyfive = new Route(Atlanta, Charleston, 0, "", 0, smallGame);
+		Route thirtysix = new Route(Atlanta, Miami, 0, "", 0, smallGame);
+		Route thirtyseven = new Route(Atlanta, Nashville, 0, "", 0, smallGame);
+		Route thirtyeight = new Route(Charleston, Raleigh, 0, "", 0, smallGame);
+		Route thirtynine = new Route(Charleston, Miami, 0, "", 0, smallGame);
+		Route fourty = new Route(Raleigh, Nashville, 0, "", 0, smallGame);
+		Route fourtyone = new Route(Raleigh, Pittsburgh, 0, "", 0, smallGame);
+		Route fourtytwo = new Route(Nashville, Pittsburgh, 0, "", 0, smallGame);
+		Route fourtythree = new Route(Washington, Pittsburgh, 0, "", 0, smallGame);
+		Route fourtyfive = new Route(Pittsburgh, Toronto, 0, "", 0, smallGame);
+		Route fourtysix = new Route(NewYork, Montreal, 0, "", 0, smallGame);
+		Route fourtyseven = new Route(Montreal, SaultStMarie, 0, "", 0, smallGame);
+		Route fourtyeight = new Route(Montreal, Toronto, 0, "", 0, smallGame);
+		Route fourtynine = new Route(SaultStMarie, Toronto, 0, "", 0, smallGame);
+		Route fifty = new Route(SaultStMarie, Winnipeg, 0, "", 0, smallGame);
+		Route fiftyone = new Route(SaultStMarie, Duluth, 0, "", 0, smallGame);
+		Route fiftytwo = new Route(Toronto, Chicago, 0, "", 0, smallGame);
+		Route fiftythree = new Route(Toronto, Duluth, 0, "", 0, smallGame);
+		Route fiftyfour = new Route(Chicago, Duluth, 0, "", 0, smallGame);
+		Route fiftyfive = new Route(Helena, Winnipeg, 0, "", 0, smallGame);
+		Route fiftysix = new Route(Helena, Duluth, 0, "", 0, smallGame);
+		Route fiftyseven = new Route(Winnipeg, Duluth, 0, "", 0, smallGame);
 		
 		this.routes.add(one);
 		this.routes.add(two);
@@ -220,139 +223,139 @@ public class PathGraph {
 		
 		//double routes
 
-		Route fourtyfour = new Route(Pittsburgh, NewYork, 0, "", 0, true);
-		Route ninetyfive = new Route(Pittsburgh, NewYork, 0, "", 0, true);
+		Route fourtyfour = new Route(Pittsburgh, NewYork, 0, "", 0, smallGame);
+		Route ninetyfive = new Route(Pittsburgh, NewYork, 0, "", 0, smallGame);
 		fourtyfour.setTwin(ninetyfive);
 		this.routes.add(fourtyfour);
 		this.routes.add(ninetyfive);
 		
-		Route fiftynine = new Route(Vancouver, Seattle, 0, "", 0, true);
-		Route sixty = new Route(Vancouver, Seattle, 0, "", 0, true);
+		Route fiftynine = new Route(Vancouver, Seattle, 0, "", 0, smallGame);
+		Route sixty = new Route(Vancouver, Seattle, 0, "", 0, smallGame);
 		fiftynine.setTwin(sixty);
 		this.routes.add(fiftynine);
 		this.routes.add(sixty);
 		
-		Route sixtyone = new Route(Seattle, Portland, 0, "", 0, true);
-		Route sixtytwo = new Route(Seattle, Portland, 0, "", 0, true);
+		Route sixtyone = new Route(Seattle, Portland, 0, "", 0, smallGame);
+		Route sixtytwo = new Route(Seattle, Portland, 0, "", 0, smallGame);
 		sixtyone.setTwin(sixtytwo);
 		this.routes.add(sixtyone);
 		this.routes.add(sixtytwo);
 		
-		Route sixtythree = new Route(Portland, SanFrancisco, 0, "", 0, true);
-		Route sixtyfour = new Route(Portland, SanFrancisco, 0, "", 0, true);
+		Route sixtythree = new Route(Portland, SanFrancisco, 0, "", 0, smallGame);
+		Route sixtyfour = new Route(Portland, SanFrancisco, 0, "", 0, smallGame);
 		sixtythree.setTwin(sixtyfour);
 		this.routes.add(sixtythree);
 		this.routes.add(sixtyfour);
 		
-		Route sixtyfive = new Route(SaltLakeCity, SanFrancisco, 0, "", 0, true);
-		Route sixtysix = new Route(SaltLakeCity, SanFrancisco, 0, "", 0, true);
+		Route sixtyfive = new Route(SaltLakeCity, SanFrancisco, 0, "", 0, smallGame);
+		Route sixtysix = new Route(SaltLakeCity, SanFrancisco, 0, "", 0, smallGame);
 		sixtyfive.setTwin(sixtysix);
 		this.routes.add(sixtyfive);
 		this.routes.add(sixtysix);
 		
-		Route sixtyseven = new Route(SaltLakeCity, Denver, 0, "", 0, true);
-		Route sixtyeight = new Route(SaltLakeCity, Denver, 0, "", 0, true);
+		Route sixtyseven = new Route(SaltLakeCity, Denver, 0, "", 0, smallGame);
+		Route sixtyeight = new Route(SaltLakeCity, Denver, 0, "", 0, smallGame);
 		sixtyseven.setTwin(sixtyeight);
 		this.routes.add(sixtyseven);
 		this.routes.add(sixtyeight);
 		
-		Route sixtynine = new Route(SanFrancisco, LosAngeles, 0, "", 0, true);
-		Route seventy = new Route(SanFrancisco, LosAngeles, 0, "", 0, true);
+		Route sixtynine = new Route(SanFrancisco, LosAngeles, 0, "", 0, smallGame);
+		Route seventy = new Route(SanFrancisco, LosAngeles, 0, "", 0, smallGame);
 		sixtynine.setTwin(seventy);
 		this.routes.add(sixtynine);
 		this.routes.add(seventy);
 		
-		Route seventyone = new Route(Houston, Dallas, 0, "", 0, true);
-		Route seventytwo = new Route(Houston, Dallas, 0, "", 0, true);
+		Route seventyone = new Route(Houston, Dallas, 0, "", 0, smallGame);
+		Route seventytwo = new Route(Houston, Dallas, 0, "", 0, smallGame);
 		seventyone.setTwin(seventytwo);
 		this.routes.add(seventyone);
 		this.routes.add(seventytwo);
 		
-		Route seventythree = new Route(Dallas, OklahomaCity, 0, "", 0, true);
-		Route seventyfour = new Route(Dallas, OklahomaCity, 0, "", 0, true);
+		Route seventythree = new Route(Dallas, OklahomaCity, 0, "", 0, smallGame);
+		Route seventyfour = new Route(Dallas, OklahomaCity, 0, "", 0, smallGame);
 		seventythree.setTwin(seventyfour);
 		this.routes.add(seventythree);
 		this.routes.add(seventyfour);
 		
-		Route seventyfive = new Route(NewOrleans, Atlanta, 0, "", 0, true);
-		Route seventysix = new Route(NewOrleans, Atlanta, 0, "", 0, true);
+		Route seventyfive = new Route(NewOrleans, Atlanta, 0, "", 0, smallGame);
+		Route seventysix = new Route(NewOrleans, Atlanta, 0, "", 0, smallGame);
 		seventyfive.setTwin(seventysix);
 		this.routes.add(seventyfive);
 		this.routes.add(seventysix);
 		
-		Route seventyseven = new Route(Denver, KansasCity, 0, "", 0, true);
-		Route seventyeight = new Route(Denver, KansasCity, 0, "", 0, true);
+		Route seventyseven = new Route(Denver, KansasCity, 0, "", 0, smallGame);
+		Route seventyeight = new Route(Denver, KansasCity, 0, "", 0, smallGame);
 		seventyseven.setTwin(seventyeight);
 		this.routes.add(seventyseven);
 		this.routes.add(seventyeight);
 		
-		Route seventynine = new Route(OklahomaCity, KansasCity, 0, "", 0, true);
-		Route eighty = new Route(OklahomaCity, KansasCity, 0, "", 0, true);
+		Route seventynine = new Route(OklahomaCity, KansasCity, 0, "", 0, smallGame);
+		Route eighty = new Route(OklahomaCity, KansasCity, 0, "", 0, smallGame);
 		seventynine.setTwin(eighty);
 		this.routes.add(seventynine);
 		this.routes.add(eighty);
 		
-		Route eightyone = new Route(StLouis, KansasCity, 0, "", 0, true);
-		Route eightytwo = new Route(StLouis, KansasCity, 0, "", 0, true);
+		Route eightyone = new Route(StLouis, KansasCity, 0, "", 0, smallGame);
+		Route eightytwo = new Route(StLouis, KansasCity, 0, "", 0, smallGame);
 		eightyone.setTwin(eightytwo);
 		this.routes.add(eightyone);
 		this.routes.add(eightytwo);
 		
-		Route eightythree = new Route(StLouis, Chicago, 0, "", 0, true);
-		Route eightyfour = new Route(StLouis, Chicago, 0, "", 0, true);
+		Route eightythree = new Route(StLouis, Chicago, 0, "", 0, smallGame);
+		Route eightyfour = new Route(StLouis, Chicago, 0, "", 0, smallGame);
 		eightythree.setTwin(eightyfour);
 		this.routes.add(eightythree);
 		this.routes.add(eightyfour);
 		
-		Route eightyfive = new Route(KansasCity, Omaha, 0, "", 0, true);
-		Route eightysix = new Route(KansasCity, Omaha, 0, "", 0, true);
+		Route eightyfive = new Route(KansasCity, Omaha, 0, "", 0, smallGame);
+		Route eightysix = new Route(KansasCity, Omaha, 0, "", 0, smallGame);
 		eightyfive.setTwin(eightysix);
 		this.routes.add(eightyfive);
 		this.routes.add(eightysix);
 		
-		Route eightyseven = new Route(Omaha, Duluth, 0, "", 0, true);
-		Route eightyeight = new Route(Omaha, Duluth, 0, "", 0, true);
+		Route eightyseven = new Route(Omaha, Duluth, 0, "", 0, smallGame);
+		Route eightyeight = new Route(Omaha, Duluth, 0, "", 0, smallGame);
 		eightyseven.setTwin(eightyeight);
 		this.routes.add(eightyseven);
 		this.routes.add(eightyeight);
 		
-		Route eightynine = new Route(Atlanta, Raleigh, 0, "", 0, true);
-		Route ninety = new Route(Atlanta, Raleigh, 0, "", 0, true);
+		Route eightynine = new Route(Atlanta, Raleigh, 0, "", 0, smallGame);
+		Route ninety = new Route(Atlanta, Raleigh, 0, "", 0, smallGame);
 		eightynine.setTwin(ninety);
 		this.routes.add(eightynine);
 		this.routes.add(ninety);
 		
-		Route ninetyone = new Route(Raleigh, Washington, 0, "", 0, true);
-		Route ninetytwo = new Route(Raleigh, Washington, 0, "", 0, true);
+		Route ninetyone = new Route(Raleigh, Washington, 0, "", 0, smallGame);
+		Route ninetytwo = new Route(Raleigh, Washington, 0, "", 0, smallGame);
 		ninetyone.setTwin(ninetytwo);
 		this.routes.add(ninetyone);
 		this.routes.add(ninetytwo);
 		
-		Route ninetythree = new Route(Washington, NewYork, 0, "", 0, true);
-		Route ninetyfour = new Route(Washington, NewYork, 0, "", 0, true);
+		Route ninetythree = new Route(Washington, NewYork, 0, "", 0, smallGame);
+		Route ninetyfour = new Route(Washington, NewYork, 0, "", 0, smallGame);
 		ninetythree.setTwin(ninetyfour);
 		this.routes.add(ninetythree);
 		this.routes.add(ninetyfour);
 		
-		Route ninetysix = new Route(Pittsburgh, Chicago, 0, "", 0, true);
-		Route ninetyseven = new Route(Pittsburgh, Chicago, 0, "", 0, true);
+		Route ninetysix = new Route(Pittsburgh, Chicago, 0, "", 0, smallGame);
+		Route ninetyseven = new Route(Pittsburgh, Chicago, 0, "", 0, smallGame);
 		ninetysix.setTwin(ninetyseven);
 		this.routes.add(ninetysix);
 		this.routes.add(ninetyseven);
 		
-		Route ninetyeight = new Route(NewYork, Boston, 0, "", 0, true);
-		Route ninetynine = new Route(NewYork, Boston, 0, "", 0, true);
+		Route ninetyeight = new Route(NewYork, Boston, 0, "", 0, smallGame);
+		Route ninetynine = new Route(NewYork, Boston, 0, "", 0, smallGame);
 		ninetyeight.setTwin(ninetynine);
 		this.routes.add(ninetyeight);
 		this.routes.add(ninetynine);
 		
-		Route onehundred = new Route(Boston, Montreal, 0, "", 0, true);
-		Route onehundredone = new Route(Boston, Montreal, 0, "", 0, true);
+		Route onehundred = new Route(Boston, Montreal, 0, "", 0, smallGame);
+		Route onehundredone = new Route(Boston, Montreal, 0, "", 0, smallGame);
 		onehundred.setTwin(onehundredone);
 		this.routes.add(onehundred);
 		this.routes.add(onehundredone);
 		
-		// TODO adjust for number of players
+		
 		
 	}
 
