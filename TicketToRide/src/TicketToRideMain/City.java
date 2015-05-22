7e79389279609cf90828702e4dd2597c86071077
Arcessimus <dsfreak17@gmail.com>
2015-05-21 23:32:05 -0400

    work on city

--- TicketToRide/src/TicketToRideMain/City.java
+++ TicketToRide/src/TicketToRideMain/City.java
@@ -3,12 +3,49 @@ package TicketToRideMain;
 import java.util.ArrayList;
 
 public class City {
-	
-	ArrayList<Route> edges;
+
 	String name;
+	ArrayList<Route> edges;
+	//ArrayList<City> adjacentCities;
 	
 	public City(String name)
 	{
 		this.name = name;
+		this.edges = new ArrayList<Route>();
+		//this.adjacentCities = new ArrayList<City>();
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public ArrayList<Route> getEdges()
+	{
+		return this.edges;
+	}
+	
+//	public ArrayList<City> getAdjacentCities()
+//	{
+//		return this.adjacentCities;
+//	}
+	
+	public void addEdge(Route e)
+	{
+		this.edges.add(e);
+	}
+	
+	public ArrayList<Route> getOwnedEdges()
+	{
+		ArrayList<Route> ownedEdges = new ArrayList<Route>();
+		for(int i = 0; i < this.edges.size(); i++)
+		{
+			if(this.edges.get(i).owned == 1)
+			{
+				ownedEdges.add(this.edges.get(i));
+			}
+		}
+		
+		return ownedEdges;
 	}
 }
--- TicketToRide/src/TicketToRideTest/CityTests.java
+++ TicketToRide/src/TicketToRideTest/CityTests.java
@@ -2,10 +2,14 @@ package TicketToRideTest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import TicketToRideMain.City;
 import TicketToRideMain.Deck;
+import TicketToRideMain.Player;
+import TicketToRideMain.Route;
 
 public class CityTests {
 
@@ -13,6 +17,31 @@ public class CityTests {
 	public void testInit() {
 		City NewYork = new City("New York City");
 		assertNotNull(NewYork);
+	}
+	
+	@Test
+	public void testGetOwnedEdges() {
+		City NewYork = new City("New York City");
+		
+		Route route1 = new Route();
+		Route route2 = new Route();
+		Route route3 = new Route();
+		Route route4 = new Route();
+		Route route5 = new Route();
+		
+		NewYork.addEdge(route1);
+		NewYork.addEdge(route2);
+		NewYork.addEdge(route3);
+		NewYork.addEdge(route4);
+		NewYork.addEdge(route5);
+		
+		Player player1 = new Player();
+		
+		route1.setOwner(player1);
+		route2.setOwner(player1);
+		
+		ArrayList<Route> ownedRoutes = NewYork.getOwnedEdges();
+		assertEquals(2, ownedRoutes.size());
 	}
 
 }
